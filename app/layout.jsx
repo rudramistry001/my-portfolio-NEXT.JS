11ded83d317218f6fbb820d0bcf48f7c4668af9e
RUDRA MISTRY <rudramistry74@yahoo.com>
2024-08-26 19:18:59 +0530

    header complete with logo

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
+// COMPONENTS
+import Header from "@/components/Header";
+
 const jetBrainsmono = JetBrains_Mono({ subsets: ["latin"], 
   weight: ["100","200","300","400", "500","600","700","800"],
 variable: '--font-jetbrainsMono'
@@ -15,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={jetBrainsmono.variable}>{children}</body>
+      <body className={jetBrainsmono.variable}>
+        <Header/>
+        
+        {children}</body>
     </html>
   );
 }
---
+++ components/Header.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white  ">
+      <div className="container mx-auto"> 
+
+        {/*logo */}
+        <Link href={"/"}>
+        <h1 className="text-4xl font-semibold">
+            Rudra Mistry
+            <span className="text-accent">.</span></h1>
+        </Link>
+
+  
+     {/*Desktop NAv */}
+     <Nav/>
+     
+      </div>
+    </header>
+  )
+}
+
+export default Header
